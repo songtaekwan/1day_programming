0e729b72b2d25d79dab221d1e72f34f553cf62ec
songtaekwan <101688746+songtaekwan@users.noreply.github.com>
2022-09-17 18:55:57 +0900

    Create Cpu0_Main.c

---
+++ 20220917/infineon/Cpu0_Main.c
@@ -0,0 +1,34 @@
+#include "Ifx_Types.h"
+#include "IfxCpu.h"
+#include "IfxScuWdt.h"
+
+IfxCpu_syncEvent g_cpuSyncEvent = 0;
+float32 test_value = 0.0;
+int core0_main(void)
+{
+    IfxCpu_enableInterrupts();
+    
+    /* !!WATCHDOG0 AND SAFETY WATCHDOG ARE DISABLED HERE!!
+     * Enable the watchdogs and service them periodically if it is required
+     */
+    IfxScuWdt_disableCpuWatchdog(IfxScuWdt_getCpuWatchdogPassword());
+    IfxScuWdt_disableSafetyWatchdog(IfxScuWdt_getSafetyWatchdogPassword());
+    
+    /* Wait for CPU sync event */
+    IfxCpu_emitEvent(&g_cpuSyncEvent);
+    IfxCpu_waitEvent(&g_cpuSyncEvent, 1);
+    
+    //initCCU6();
+    //startPWMGeneration();
+
+    initGpt12PWM();
+
+    runGpt12PWM();
+
+    Set_GptPWM_Duty(30);
+
+    while(1)
+    {
+    }
+    return (1);
+}
