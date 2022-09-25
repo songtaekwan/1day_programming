7fb0b678ae84d74ab70c3ba14724bc3e22f0d3fd
songtaekwan <101688746+songtaekwan@users.noreply.github.com>
2022-09-25 17:28:13 +0900

    Create Cpu0_Main.c

---
+++ 20220921_Motor_Control/Cpu0_Main.c
@@ -0,0 +1,64 @@
+#include "Ifx_Types.h"
+#include "IfxCpu.h"
+#include "IfxScuWdt.h"
+#include "ADC_Single_Channel.h"
+#include "Bsp.h"
+#include "Motor_control_pwm.h"
+#include "GPT12_PWM_Generation.h"
+
+#define WAIT_TIME   1000            
+
+IfxCpu_syncEvent g_cpuSyncEvent = 0;
+/*--------pwm control---------*/
+int adc_value;
+int pre_val = 0;
+int val = 0;
+int duty;
+float alpha = 0.2;
+int pwm;
+
+union
+{
+    int data;
+    char bytedata[4];
+} m_adc_value;
+
+int core0_main(void)
+{
+    IfxCpu_enableInterrupts();
+    
+    IfxScuWdt_disableCpuWatchdog(IfxScuWdt_getCpuWatchdogPassword());
+    IfxScuWdt_disableSafetyWatchdog(IfxScuWdt_getSafetyWatchdogPassword());
+    
+    /* Wait for CPU sync event */
+    IfxCpu_emitEvent(&g_cpuSyncEvent);
+    IfxCpu_waitEvent(&g_cpuSyncEvent, 1);
+
+    vadcBackgroundScanInit();
+
+    /* Start the background scan */
+    vadcBackgroundScanRun();
+
+    /* Initialize UART */
+
+    L298_Pin_init();
+    initGpt12PWM();
+    runGpt12PWM();
+
+    while(1)
+    {
+        /*-------filter--------*/
+        adc_value = indicateConversionValue();
+        val = alpha * pre_val + (1 - alpha) * adc_value;
+        pre_val = val;
+
+        /*------duty, pwm-----*/
+        duty = (val*100)/4095;
+        pwm = (val*255)/4095;
+
+        Set_GptPWM_Duty(duty);
+        motor_control(pwm);
+        //wait(ticksFor100ms);
+    }
+    return (1);
+}
